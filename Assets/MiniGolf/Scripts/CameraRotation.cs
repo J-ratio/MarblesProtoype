132ec5a6939a99b9678d7037e005059f9f02900b
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-03-14 12:38:48 +0530

    Initial commit

---
+++ Assets/MiniGolf/Scripts/CameraRotation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script which makes camera rotate around ball
+/// </summary>
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 0.2f;    //rotation speed
+    int rotationDirection;
+
+    public static CameraRotation instance;
+
+    Coroutine RotationInstance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartRotatingCamera(bool toLeft)           
+    {
+        if(toLeft) rotationDirection = 1; else rotationDirection = -1;
+
+        if(RotationInstance == null)    RotationInstance = StartCoroutine("RotateCam");
+    }
+
+    public void StopRotating()
+    {
+
+        if(RotationInstance != null){
+            StopCoroutine(RotationInstance);
+            RotationInstance = null;
+        }
+    }
+
+    IEnumerator RotateCam()
+    {
+        while(true)
+        {
+            transform.Rotate(rotationDirection*Vector3.down, rotationSpeed); //rotate the camera
+            yield return new WaitForEndOfFrame();
+        }
+        yield break;
+    }
+
+}
